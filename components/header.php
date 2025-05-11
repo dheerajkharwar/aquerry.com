59b276254c668cb20f3f5dfb9a6309cb3089c123
Dheeraj Kharwar <100955259+dheerajkharwar@users.noreply.github.com>
2025-05-11 18:29:17 +0530

    ghjk

--- components/header.php
+++ components/header.php
@@ -43,8 +43,8 @@
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
             <a href="" class="navbar-brand p-0">
-                <h1 class="text-primary"><i class="fas fa-hand-holding-water me-3"></i>Meenakanchan Pure H2O Private Limited</h1>
-                <img src="img/logo.png" alt="Logo">
+                <!-- <h1 class="text-primary"><i class="fas fa-hand-holding-water me-3"></i>Meenakanchan Pure H2O Private Limited</h1> -->
+                <img src="logo.png" alt="Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars"></span>
