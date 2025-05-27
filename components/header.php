95b33ae077ba211e12851a9060bf7c2533c21187
Dheeraj Kharwar <100955259+dheerajkharwar@users.noreply.github.com>
2025-05-27 09:03:29 +0530

    ghjk

--- components/header.php
+++ components/header.php
@@ -51,9 +51,9 @@ header("Expires: 0");
                 <!-- <h1 class="text-primary"><i class="fas fa-hand-holding-water me-3"></i>Meenakanchan Pure H2O Private Limited</h1> -->
                 <img src="logo.png" alt="Logo">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars"></span>
-            </button>
+            </button> -->
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <!-- <div class="navbar-nav ms-auto py-0"> -->
                 <!-- <a href="index.php" class="nav-item nav-link">Home</a>
